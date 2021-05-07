92f3a6c0c18cee2adbd95b7c13b4026c9a3e8abe
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-06 23:42:01 -0400

    Updated server.js, added url model, updated dependencies

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const urlSchema = require('./models/url');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+mongoose.connect('mongodb://localhost/db', {
+  // TODO: fix connection error
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// eslint-disable-next-line no-unused-vars
+app.get('/', async (req, res) => {
+  const urls = await urlSchema.find();
+  console.log(urls);
+});
+
+app.post('/create', async (req, res) => {
+  const requestBody = req.body;
+
+  // TODO: check if shortUrl already exists and return error if it does
+
+  await urlSchema.create({
+    full: requestBody.fullUrl,
+    short: requestBody.shortUrl
+  });
+
+  res.redirect('/');
 });
 
 // eslint-disable-next-line no-undef
