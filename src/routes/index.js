95b5c1655210cdc4e696e8a0b1010fd2a13c7a93
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-10 10:44:07 -0400

    Updated index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,13 @@ const router = express.Router();
 
 module.exports = router.get('/', async (req, res) => {
   const urls = await Url.find();
-  res.json(urls);
+  const responseJson = [];
+  urls.forEach((url) => {
+    responseJson.push({
+      full: url.full,
+      short: url.short,
+      clicks: url.clicks,
+    });
+  });
+  res.json(responseJson);
 });
