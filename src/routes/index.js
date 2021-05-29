c1ab3d9fc0933d0eee5c733df22f8a0cb6179d5f
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-28 22:32:09 -0400

    Removed intermediate endpoint `create`

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,4 @@
-const { default: axios } = require('axios');
 const express = require('express');
-const { BASE_URL } = require('../constants');
 const Url = require('../models/Url');
 
 const router = express.Router();
@@ -8,14 +6,4 @@ const router = express.Router();
 module.exports = router.get('/', async (req, res) => {
   const urls = await Url.find();
   res.render('index.ejs', { urls });
-});
-
-module.exports = router.post('/', async (req, res) => {
-  try {
-    await axios.post(BASE_URL + 'api/create/', req.body);
-  } catch (error) {
-    // eslint-disable-next-line no-undef
-    console.error(error);
-  }
-  res.redirect(BASE_URL);
 });
