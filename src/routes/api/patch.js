77a226ff9894b123b2b12fe685038d1571fff33f
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-29 03:31:16 -0400

    Update patch URL route, remove ability to edit short URL

--- src/routes/api/patch.js
+++ src/routes/api/patch.js
@@ -8,31 +8,29 @@ const router = express.Router();
 
 module.exports = router.patch('/', async (req, res, next) => {
   if (req.body.shortUrl === undefined) {
-    return next(createError(statusCode.UNPROCESSABLE_ENTITY, 'shortUrl is required.'));
+    return next(
+      createError(statusCode.UNPROCESSABLE_ENTITY, 'shortUrl is required.')
+    );
   }
 
   let url = await Url.findOne({ short: req.body.shortUrl });
 
   if (url === null) {
-    return next(createError(statusCode.UNPROCESSABLE_ENTITY, 'shortUrl does not exist.'));
+    return next(
+      createError(statusCode.UNPROCESSABLE_ENTITY, 'shortUrl does not exist.')
+    );
   }
-  if (req.body.newName === undefined && req.body.newShortUrl === undefined && req.body.newFullUrl === undefined) {
+  if (req.body.newName === undefined && req.body.newFullUrl === undefined) {
     return next(
       createError(
         statusCode.BAD_REQUEST,
-        'No new values for newName, newShortUrl or newFullUrl were provided. At least 1 new value is required.'
+        'No values for newName or newFullUrl were provided. At least 1 value is required.'
       )
     );
-  }
-  if (req.body.newShortUrl !== undefined && (await doesShortUrlExist(req.body.shortUrl))) {
-    return next(createError(statusCode.CONFLICT, 'shortUrl already exists.'));
   }
 
   if (req.body.newName !== undefined) {
     url.name = req.body.newName;
-  }
-  if (req.body.newShortUrl !== undefined) {
-    url.short = req.body.newShortUrl;
   }
   if (req.body.newFullUrl !== undefined) {
     url.full = req.body.newFullUrl;
