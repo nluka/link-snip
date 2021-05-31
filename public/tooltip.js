98e90937c1c13d997dae7dad127629f4cf6a6bba
Nicholas Lukasevich <n.luka784@gmail.com>
2021-05-31 16:06:25 -0400

    Update short URL copy button

---
+++ public/tooltip.js
@@ -0,0 +1,6 @@
+const tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
